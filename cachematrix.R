5fdab00f5f5622248bcce0391ea10d5d58748ac7
djul <34135473+jnabonne@users.noreply.github.com>
2018-04-04 19:50:14 +0200

    Update cachematrix.R
    Complete code for JHU R Programming 2 Assignment (Lexical Scoping)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Creates special matrix (~ a list) containing functions to
+#   - set / get the value of the matrix
+#   - set / get the inverse of the matrix
+makeCacheMatrix <- function(m = matrix()) {
+    minv <- NULL
+    set <- function(new_val) {
+        m <<- new_val
+        minv <<- NULL
+    }
+    get <- function() m
+    setinv <- function(inv) minv <<- inv
+    getinv <- function() minv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Checks if special matrix's inverse has already been calculated and return it
+# otherwise, calculates it and stores it  in the cache via setinv function
+cacheSolve <- function(m, ...) {
+    minv <- m$getinv()
+    if(!is.null(minv)) {
+        message("getting cached matrix inverse")
+        return(minv)
+    }
+    data <- m$get()
+    minv <- solve(data)
+    m$setinv(minv)
+    minv
 }
